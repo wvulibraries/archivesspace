9e2f7ab46da6fb5c8054e2ae7bd8a82a092eaa86
Michael Bond <mbond@the-forgotten.org>
2016-03-02 13:27:33 -0500

    download archivesspace instead of having it in the repo (too big)

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,13 +5,14 @@ yum groupinstall -y "Development Tools"
 yum -y install \
 	emacs emacs-common emacs-nox git wget \
 	screen curl-devel openssl-devel readline-devel \
-	java-1.7.0
+	java-1.7.0 wget
 
 rpm -Uvh http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
 yum -y install mysql-community-server mysql-community-client mysql-community-devel mysql-community-common mysql-community-libs mysql-community-release
 
 cp /vagrant/archivesspace-v1.3.0.zip /home/
 cd /home/
+wget https://github.com/archivesspace/archivesspace/releases/download/v1.3.0/archivesspace-v1.3.0.zip
 unzip archivesspace-v1.3.0.zip
 rm -f archivesspace-v1.3.0.zip
 
@@ -29,7 +30,7 @@ systemctl start mysqld
 mysql -u root < /vagrant/mysqlSetup.sql
 bash /home/archivesspace/scripts/setup-database.sh
 
-/usr/sbin/useradd -M archivesspace 
+/usr/sbin/useradd -M archivesspace
 chown archivesspace /home/archivesspace/ -R
 
 cd /etc/init.d
