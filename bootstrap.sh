f9d8aca1e6ca9b7b7775e387dbb4b42179f922e8
Michael Bond <mbond@the-forgotten.org>
2016-10-27 19:39:04 -0400

    Increase version to 1.5.1 -- breaks vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=1.4.2
+VERSION=1.5.1
 
 yum groupinstall -y "Development Tools"
 
@@ -13,15 +13,15 @@ rpm -Uvh http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
 yum -y install mysql-community-server mysql-community-client mysql-community-devel mysql-community-common mysql-community-libs mysql-community-release cabextract
 rpm -Uvh msttcore-fonts-installer-2.6-1.noarch.rpm
 
-cp /vagrant/archivesspace-v$VERSION.zip /home/
+cp /opt/archivesspace/archivesspace-v$VERSION.zip /home/
 cd /home/
 wget https://github.com/archivesspace/archivesspace/releases/download/v$VERSION/archivesspace-v$VERSION.zip
 unzip archivesspace-v$VERSION.zip
 rm -f archivesspace-v$VERSION.zip
 
 ## copy over local configs
-cp -f /vagrant/config.rb /home/archivesspace/config/config.rb
-cp -f /vagrant/archivesspace.sh /home/archivesspace/config/archivesspace.sh
+cp -f /opt/archivesspace/config.rb /home/archivesspace/config/config.rb
+cp -f /opt/archivesspace/archivesspace.sh /home/archivesspace/config/archivesspace.sh
 
 ## setup mysql
 cd /home/archivesspace/lib
@@ -30,7 +30,7 @@ curl -Oq http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.36/mysql
 systemctl enable mysqld
 systemctl start mysqld
 
-mysql -u root < /vagrant/mysqlSetup.sql
+mysql -u root < /opt/archivesspace/mysqlSetup.sql
 bash /home/archivesspace/scripts/setup-database.sh
 
 /usr/sbin/useradd -M archivesspace
